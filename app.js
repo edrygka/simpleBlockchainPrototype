d10ef3e4bf8ab1ba16a336e7bd19b561a573e163
Eduard <macbook@MacBook-Pro-Macbook.local>
2018-02-15 19:31:55 +0200

    API interface with correct working and handling functions, involve some optimizations

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const Acc = low(forAcc)
 const p2p_port = process.env.P2P_PORT || 3001
 const http_port = process.env.HTTP_PORT || 3000
 //const initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : []
-const AxelString = "I love AXEL coin"
+const AS = "I love AXEL coin"
 const event = new events.EventEmitter() 
 
 //cryptography
@@ -28,21 +28,16 @@ TX.defaults({ transactions: []})
 Acc.defaults({ accounts: []})
     .write()
 
-//get latest TX record
-function getLastTX(){
-    let count = TX.get('transactions')
-      .size()
-      .value()
-    return TX.get(`transactions[${count-1}]`).value()
-}
+const checkTXDatabase = () => TX.get('transactions').size().value()
+
+const checkAcDatabase = () => Acc.get('accounts').size().value()
+
+// Get last transaction record
+const getLastTX = () => TX.get(`transactions[${checkTXDatabase()-1}]`).value()
 
 // Get last account record
-function getLastAc(){
-    let count = Acc.get('accounts')
-        .size()
-        .value()
-    return Acc.get(`accounts[${count-1}]`).value()
-}
+const getLastAc = () => Acc.get(`accounts[${checkAcDatabase()-1}]`).value()
+
 
 // get balance by account
 async function getBalanceByAccount(_address){
@@ -66,10 +61,26 @@ function init_p2p_server(){
     // need to validate signature of transaction
 
     event.on('newTX', data => {
-        if(verifySignature(AxelString, data.pubKey, data.sign) == true){
-            createTx(data.recipient, data.publicKey, data.amount)
-            
-        }
+        // Get new purpose for transaction
+        const sign = String(data.sign)
+        const from = String(data.from)
+        const amount = Number(data.amount)
+        const to = String(data.to)
+        //Check if we have the same account in database
+        checkAccountForExist(from)
+        //If signature is valid and address has enough coins
+        checkForEnoughCoins(from, amount, result => {
+            if(result == true){
+                // verify sender's signature
+                verifySignature(AS, from, sign, res => {
+                    if(res == true){
+                        checkAccountForExist(to)
+                        // compare transaction and push in blockchain
+                        createTx(to, from, amount)
+                    }
+                })
+            }
+        })
     })
 
     server.listen(p2p_port, () => console.log('Listening p2p server on port: ' + p2p_port))
@@ -86,7 +97,7 @@ function init_webinterface(){
     app.post('/createTX', urlencodedParser, (req, res) => {
         if(!req.body) return res.sendStatus(400)
         //sign TX
-        event.emit('newTX', {sign: "signature", publicKey: req.body.pubKey, amount: req.body.amount, recipient: req.body.recipAddr})
+        event.emit('newTX', {sign: "signature", from: req.body.pubKey, amount: req.body.amount, to: req.body.recipAddr})
         res.send("Your transaction will be broadcast...")
     })
 
@@ -104,6 +115,7 @@ function init_webinterface(){
     server.listen(http_port, () => console.log('Listening http on port: ' + http_port))
     //====================================genesis function=========================================
     genesisStart(10000)
+    //createTransaction("genesis", 1000000)
 }
 
 // create Key pair
@@ -119,23 +131,27 @@ function createKeyPair(callback){
 
 // start genesis
 function genesisStart(_amount){
-    createKeyPair(result => {
-        //TODO: FIX THIS КОСТЫЛЬ
-        let index = 0, id = 0
-        let prevHash = CryptoJS.SHA256(0).toString()
-        //let sign = "signature"
-        let time = new Date()
-        let pubKey = result.pubKey
-        let from = "genesis"
-        let hash = calculateTxHash(index, prevHash, time, pubKey, from, _amount)
-        console.log(`\n Genesis start \n public key = ${result.pubKey} \n private key = ${result.privKey}\n`)
-        TX.get('transactions')
-            .push({index: index, prevHash: prevHash, time: time, to: pubKey, from: from, amount: _amount, hash: hash})
-            .write()
-        Acc.get('accounts')
-            .push({id: id, address: pubKey, balance: _amount})
-            .write()
-    })
+    //check is database empty
+    if(checkTXDatabase() == 0){
+        createKeyPair(async result => {
+            //TODO: FIX THIS КОСТЫЛЬ
+            let index = 0, id = 0
+            let prevHash = CryptoJS.SHA256(0).toString()
+            //let sign = "signature"
+            let time = new Date()
+            let pubKey = result.pubKey
+            let from = "genesis"
+            let hash = calculateTxHash(index, prevHash, time, pubKey, from, _amount)
+            TX.get('transactions')
+                .push({index: index, prevHash: prevHash, time: time, to: pubKey, from: from, amount: _amount, hash: hash})
+                .write()
+            Acc.get('accounts')
+                .push({id: id, address: pubKey, balance: _amount})
+                .write()
+            const balance = await getBalanceByAccount(pubKey)
+            console.log(`\n Genesis start \n public key = ${result.pubKey} \n private key = ${result.privKey}\n and balance = ${balance} AXELs \n`)
+        })
+    }
 }
 
 // create new account record if not exist
@@ -150,6 +166,62 @@ async function checkAccountForExist(_address){
     }
 }
 
+// to create an overload functions
+function polymorph() {
+    var len2func = [];
+    for(var i=0; i<arguments.length; i++)
+      if(typeof(arguments[i]) == "function")
+        len2func[arguments[i].length] = arguments[i];
+    return () => {
+      return len2func[arguments.length].apply(this, arguments);
+    }
+}
+
+// const createTransaction = polymorph(
+//     (_genesis, _amount) => {
+//         // create keys for first account
+//         createKeyPair(async result => {
+//             const index = 0
+//             const prevHash = CryptoJS.SHA256(0).toString()
+//             const time = new Date()
+//             const pubKey = result.pubkey
+//             const hash = calculateTxHash(index, prevHash, time, pubKey, _genesis, _amount)
+//             //push transaction into database
+//             TX.get('transactions')
+//                 .push({index: index, prevHash: prevHash, time: time, to: pubKey, from: _genesis, amount: _amount, hash: hash})
+//                 .write()
+//             //push account record with amount
+//             Acc.get('accounts')
+//                 .push({id: 0, address: pubKey, balance: _amount})
+//                 .write()
+//             const balance = await getBalanceByAccount(pubKey)
+//             console.log(`\n Genesis start \n public key = ${result.pubKey} \n private key = ${result.privKey}\n and balance = ${balance} AXELs \n`)
+//         })
+//     },
+//     (_to, _from, _amount) => {
+//         const index = getLastTX().index + 1
+//         const prevHash = getLastTX().hash
+//         const time = new Date()
+//         const amount = Number(_amount)
+//         const hash = calculateTxHash(index, prevHash, time, _to, _from, amount)
+//         TX.get('transactions')
+//             .push({index: index, prevHash: prevHash, time: time, to: _to, from: _from, amount: amount, hash: hash})
+//             .write()
+//         getBalanceByAccount(_from).then(balance_from => {
+//             let _id = getLastAc().id + 1
+//             Acc.get('accounts')
+//                 .push({id: _id, address: _from, balance: balance_from - amount})
+//                 .write()
+//             getBalanceByAccount(_to).then(balance_to => {
+//                 _id = getLastAc().id + 1
+//                 Acc.get('accounts')
+//                     .push({id: _id, address: _to, balance: balance_to + amount})
+//                     .write()
+//             })
+//         })
+//     }
+// )
+
 // create transaction
 function createTx(_to, _from, _amount){
     //TODO: add signature param
@@ -158,31 +230,38 @@ function createTx(_to, _from, _amount){
     let _time = new Date()
     let amount = Number(_amount)
     let _hash = calculateTxHash(_index, _prevHash, _time, _to, _from, _amount)
-    checkAccountForExist(_from)
-    getBalanceByAccount(_from).then((balance_from) => {
-        //if account exist and have enough money
-        if(balance_from >= amount){
-            checkAccountForExist(_to)
-            TX.get('transactions')
-                .push({index: _index, prevHash: _prevHash, time: _time, to: _to, from: _from, amount: amount, hash: _hash})
-                .write()
-            let _id = getLastAc().id + 1
+    // push transaction in database
+    TX.get('transactions')
+        .push({index: _index, prevHash: _prevHash, time: _time, to: _to, from: _from, amount: amount, hash: _hash})
+        .write()
+    // decrease sender's balance
+    getBalanceByAccount(_from).then(balance_from => {
+        let _id = getLastAc().id + 1
+        Acc.get('accounts')
+            .push({id: _id, address: _from, balance: balance_from - amount})
+            .write()
+        // increase recipient balance
+        getBalanceByAccount(_to).then(balance_to => {
+            _id = getLastAc().id + 1
             Acc.get('accounts')
-                .push({id: _id, address: _from, balance: balance_from - amount})
+                .push({id: _id, address: _to, balance: balance_to + amount})
                 .write()
-            getBalanceByAccount(_to).then(balance_to => {
-                _id = getLastAc().id + 1
-                Acc.get('accounts')
-                    .push({id: _id, address: _to, balance: balance_to + amount})
-                    .write()
-            })
-        }
-        // if account not exist and would be created
-    })  
+        })
+    })
 }
 
-function verifySignature(msg, pubKey, sign){
-    return true
+// check that sender has enough money
+function checkForEnoughCoins(_pubkey, _amount, callback){
+    const from = _pubkey
+    const amount = Number(_amount)
+    getBalanceByAccount(from).then(balance_from => {
+        if(balance_from >= amount) return callback(true)
+        return callback(false)
+    })
+}
+
+function verifySignature(msg, pubKey, sign, callback){
+    return callback(true)
 }
 
 function IOValidator(pubkey){
@@ -191,9 +270,7 @@ function IOValidator(pubkey){
 }
 
 // calculate transaction hash
-function calculateTxHash(index, previousHash, timestamp, sign, to, from, amount){
-    return CryptoJS.SHA256(index + previousHash + timestamp + sign + to + from + amount).toString()
-}
+const calculateTxHash = (index, previousHash, timestamp, sign, to, from, amount) => CryptoJS.SHA256(index + previousHash + timestamp + sign + to + from + amount).toString()
 
 init_p2p_server()
 init_webinterface()
